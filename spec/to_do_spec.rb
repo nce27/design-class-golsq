9a5805a2658412a1974b6c4fcc0a339bb028a4fc
nce27 <nc3elliott@gmail.com>
2023-05-19 10:39:29 +0100

    Updated the spec file to add a test when given two tasks.

--- spec/to_do_spec.rb
+++ spec/to_do_spec.rb
@@ -14,6 +14,16 @@ RSpec.describe TodoList do
       expect(todo_list.list).to eq ["Wash the car"]
     end
   end
+
+  context "given two tasks" do
+    it "has both tasks in the list" do
+      todo_list = TodoList.new 
+      todo_list.add("Wash the car")
+      todo_list.add("Wash the dog")
+      expect(todo_list.list).to eq ["Wash the car", "Wash the dog"]
+    end
+  end
+  
   context "when we complete given tasks" do
     xit "has two tasks in the list" do
       todo_list = TodoList.new
