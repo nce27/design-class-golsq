60f89ecd6136d40c7e0fb022e2dda17c5cb3b4eb
nce27 <nc3elliott@gmail.com>
2023-05-19 14:21:22 +0100

    Updated the spec file so that it creates an empty tracklist.

--- spec/music_tracker_spec.rb
+++ spec/music_tracker_spec.rb
@@ -3,7 +3,8 @@ require "music_tracker"
 RSpec.describe Music do
   it "creates an empty tracklist" do
     music_tracks = Music.new
-    music_tracks.tracklist
+    expect(music_tracks.tracklist).to eq []
+    
   end
 end
 
