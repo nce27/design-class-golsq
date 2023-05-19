4ef137cdb5cb5116e6ddab22171e4f37742697b8
nce27 <nc3elliott@gmail.com>
2023-05-19 14:32:30 +0100

    Updated to add one track to the tracklist when given a song.

--- spec/music_tracker_spec.rb
+++ spec/music_tracker_spec.rb
@@ -1,10 +1,22 @@
 require "music_tracker"
 
 RSpec.describe Music do
-  it "creates an empty tracklist" do
-    music_tracks = Music.new
-    expect(music_tracks.tracklist).to eq []
-    
+  context "when given no tracks" do
+    it "creates an empty tracklist" do
+      music_tracks = Music.new
+      expect(music_tracks.tracklist).to eq []
+    end
+  end
+
+  describe "#music_tracks" do
+    context "when given a track" do
+      it "adds to tracklist" do
+        music_tracks = Music.new
+        music_tracks.add("Can't stop coding")
+        expect(music_tracks.tracklist).to eq ["Can't stop coding"]
+      end
+
+    end
   end
 end
 
